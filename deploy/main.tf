3a07c29e64c32e70a9bb6c5931028fbd6145f9f6
Mark Winterbottom <mark@winterbottom.me>
2021-03-30 17:51:18 +0100

    Initial commit.

---
+++ deploy/main.tf
@@ -0,0 +1,8 @@
+# Put TF code for bastion instance here
+# Note: This may look different from the video because I left out the backend
+#       configuration for simplicity.
+
+provider "aws" {
+    region = "us-east-1"
+    version = "~> 2.54.0"
+}
